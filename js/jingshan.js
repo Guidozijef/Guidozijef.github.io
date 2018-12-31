1d38f70af6b0ef5219a2272d204863f527f6100f
Guidozijef <913497146@qq.com>
2018-12-31 21:08:46 +0800

    Site updated: 2018-12-31 21:08:38

--- js/jingshan.js
+++ js/jingshan.js
@@ -1,10 +1,11 @@
 $(function() {
 	$.ajax({
 		type: 'get',
-		url: 'http://open.iciba.com/dsapi/',
-		data: 'data',
-		dataType: 'dataType',
-		success: function(response) {
+		url: 'open.iciba.com/dsapi/',
+		async: true,
+		dataType: 'jsonp',
+		jsonpCallback: 'info',
+		success: function info(response) {
 			console(response);
 			// $('.jingshan .jingshan-img img').src = response.picture2;
 			$('.jingshan .jingshan-img img').attr({ src: response.picture2 });
