1155b1f44793edce2add555b1a4eefdbaf8ef6dd
Guidozijef <913497146@qq.com>
2019-01-09 12:04:11 +0800

    Site updated: 2019-01-09 12:03:55

--- js/jingshan.js
+++ js/jingshan.js
@@ -10,6 +10,9 @@ $(function() {
 			// document.getElementById('jingshan-img').src = response.picture2; // 原生js
 			$('.jingshan .jingshan-img img').attr({ src: response.picture2 }); // jquery
 			$('.jingshan-content p').html(response.content);
+			$('.jingshan-content p img').click(function(e) {
+				$('.jingshan .voice').attr({ src: response.tts });
+			});
 			$('.jingshan-content span').html(response.note);
 		}
 	});
